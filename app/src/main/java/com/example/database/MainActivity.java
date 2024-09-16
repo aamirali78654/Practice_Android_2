19ced16a51905c13a7412edc270410b2d2fc6906
aamirali78654 <aamiralisargani786@gmail.com>
2024-09-16 18:15:01 +0500

    xml file image and button

---
+++ app/src/main/java/com/example/database/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.database;
+
+import android.os.Bundle;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+    }
+}
